f956b457afcca7aa4c9fd950fd30d0c52a415dce
Andrei K <1____1@bk.ru>
2020-01-14 14:41:23 +0300

    add save book

--- parser.py
+++ parser.py
@@ -1,15 +1,25 @@
+import os
 import requests
 
 
-def download_book(id_):
+def fetch_book(id_):
     url = f'http://tululu.org/txt.php?id={id_}'
     response = requests.get(url)
     response.raise_for_status()
     if url == response.url:
-        with open(f'{id_}.txt','w') as f:
-            f.write(response.text)
+        return response.text
+    return None
+
+
+def save_book_to_folder(book, folder, id_):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open(f'{folder}/id{id_}.txt','w') as f:
+        f.write(book)
 
 
 if __name__ == "__main__":
     for num in range(1,11):
-        download_book(num)
+        book = fetch_book(num)
+        if book:
+            save_book_to_folder(book, 'books', num)
