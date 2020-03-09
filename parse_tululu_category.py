58fbe9e3260d3de28b9a0de72fb3a0dc45c22dea
Andrei K <1____1@bk.ru>
2020-03-09 17:51:33 +0300

    black

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -14,7 +14,7 @@ def fetch_book_soup(url):
     response = requests.get(url)
     response.raise_for_status()
     if url == response.url:
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, "lxml")
         return soup
     return None
 
@@ -24,21 +24,23 @@ def fetch_books_urls(url):
     base_url = "http://tululu.org/"
     book_soup = fetch_book_soup(url)
     book_cards = book_soup.select(".d_book")
-    books_urls = [urljoin(base_url, book_card.select_one("a").get('href')) 
-            for book_card in book_cards]
+    books_urls = [
+        urljoin(base_url, book_card.select_one("a").get("href"))
+        for book_card in book_cards
+    ]
     return books_urls
 
 
 def parse_pages(start_page, end_page):
     books_urls = []
-    for page in range(start_page, end_page+1):
+    for page in range(start_page, end_page + 1):
         url = f"http://tululu.org/l55/{page}/"
         books_urls += fetch_books_urls(url)
     return books_urls
 
 
 def get_book_id(book_url):
-    return book_url.split('/')[-2][1:]
+    return book_url.split("/")[-2][1:]
 
 
 def fetch_books_ids(start_page, end_page):
@@ -48,11 +50,10 @@ def fetch_books_ids(start_page, end_page):
 
 
 def parse_args():
-    parser = ArgParser(default_config_files=['.env'])
+    parser = ArgParser(default_config_files=[".env"])
     parser.add("--start_page", required=True, type=int)
     parser.add("--end_page", type=int, default=701)
-    parser.add("--file", required=True,
-               help="books description file")
+    parser.add("--file", required=True, help="books description file")
     args = parser.parse_args()
     return args
 
@@ -63,20 +64,21 @@ def main():
     start_page, end_page, description_file = args.start_page, args.end_page, args.file
     books_ids = fetch_books_ids(start_page, end_page)
     for book_id in books_ids:
-        book_txt_url = f'http://tululu.org/txt.php?id={book_id}'
+        book_txt_url = f"http://tululu.org/txt.php?id={book_id}"
         book_page_url = f"http://tululu.org/b{book_id}/"
         book_soup = fetch_book_soup(book_page_url)
-        if not  book_soup:
+        if not book_soup:
             continue
         book_name = fetch_book_title(book_soup)
         book_cover_url = fetch_book_cover_url(book_soup, book_page_url)
-        book_description = {"title": fetch_book_title(book_soup),
-                            "author": fetch_book_author(book_soup),
-                            "img_src": download_image(book_cover_url),
-                            "book_path": download_txt(book_txt_url, book_name),
-                            "comments": fetch_book_comments(book_soup),
-                            "genre": fetch_book_genre(book_soup),
-                            }
+        book_description = {
+            "title": fetch_book_title(book_soup),
+            "author": fetch_book_author(book_soup),
+            "img_src": download_image(book_cover_url),
+            "book_path": download_txt(book_txt_url, book_name),
+            "comments": fetch_book_comments(book_soup),
+            "genre": fetch_book_genre(book_soup),
+        }
         books_description.append(book_description)
     save_books_description(books_description, description_file)
 
